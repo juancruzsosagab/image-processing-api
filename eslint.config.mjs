00965c0f39e08b750fa37dffed097b9c1e7cec4c
Juan <juancruz.sosag@gmail.com>
2025-07-03 23:44:42 +0200

    feat(eslint): add eslint-plugin-jest and update eslint version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,9 +26,15 @@ export default tseslint.config(
   },
   {
     rules: {
+      ...require('eslint-plugin-jest').configs.recommended.rules,
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+  {
+    plugins: {
+      jest: require('eslint-plugin-jest'),
     },
   },
 );
