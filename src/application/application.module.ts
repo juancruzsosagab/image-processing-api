0d462f46c0e0ce8fa05cbf8c04d3a05af61fd6fd
Juan <juancruz.sosag@gmail.com>
2025-07-03 19:59:28 +0200

    feat(application): add ApplicationModule to provide task use cases

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CreateTaskUseCase } from './use-cases/create-task.usecase';
+import { GetTaskUseCase } from './use-cases/get-task.usecase';
+import { InfrastructureModule } from '../infrastructure/infrastructure.module';
 
-@Module({})
+@Module({
+  imports: [InfrastructureModule],
+  providers: [CreateTaskUseCase, GetTaskUseCase],
+  exports: [CreateTaskUseCase, GetTaskUseCase],
+})
 export class ApplicationModule {}
